fdfce4d8c8bd1e21ee39927015676598ba730179
Artur <Karturokga@gmail.com>
2024-10-22 17:16:19 +0300

    Test2

--- WPFTestTreug/MainWindow.xaml.cs
+++ WPFTestTreug/MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace WPFTestTreug
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -28,70 +26,56 @@ namespace WPFTestTreug
         private void Test_Click(object sender, RoutedEventArgs e)
         {
             int a, b, c;
+            string photo;
             try
             {
                 a = Convert.ToInt32(A.Text);
                 b = Convert.ToInt32(B.Text);
                 c = Convert.ToInt32(C.Text);
+                if (a < 0 || b < 0 || c < 0)
+                {
+                    Result.Text = "Числа должны быть положительными";
+                    TR.Source = null;
+                    return;
+                }
             }
             catch (Exception ex)
             {
                 Result.Text = ex.Message;
+                TR.Source = null;
                 return;
             }
+            A.Text = Convert.ToString(a);
+            B.Text = Convert.ToString(b);
+            C.Text = Convert.ToString(c);
+
+            if (a + b < c || a + c < b || b + c < a || a < 0 || b < 0 || c < 0)
+            {
+                Result.Text = "Треугольник не существует ";
+                TR.Source = null;
+
+                photo = Environment.CurrentDirectory + "\\N.png";
+            }
+            else
             if (a == b && b == c)
             {
                 Result.Text = "Равносторонний треугольник";
-                string photo = Environment.CurrentDirectory + "\\RS.png";
-                Uri uri = new Uri(photo, UriKind.Absolute);
-                BitmapImage bitmapImage = new BitmapImage(uri);
-                TR.Source = bitmapImage;
-            }else
-            if (a == c && b != a && b != c)
-            {
-                Result.Text = "Равнобедренный треугольник";
-                string photo = Environment.CurrentDirectory + "\\RB.png";
-                Uri uri = new Uri(photo, UriKind.Absolute);
-                BitmapImage bitmapImage = new BitmapImage(uri);
-                TR.Source = bitmapImage;
-            }else
-            if (a == b && c != a && c != b)
-            {
-                Result.Text = "Равнобедренный треугольник";
-                string photo = Environment.CurrentDirectory + "\\RB.png";
-                Uri uri = new Uri(photo, UriKind.Absolute);
-                BitmapImage bitmapImage = new BitmapImage(uri);
-                TR.Source = bitmapImage;
-            }else
-            if(b == c && a != c && a != b)
-            {
-                Result.Text = "Равнобедренный треугольник";
-                string photo = Environment.CurrentDirectory + "\\RB.png";
-                Uri uri = new Uri(photo, UriKind.Absolute);
-                BitmapImage bitmapImage = new BitmapImage(uri);
-                TR.Source = bitmapImage;
+                photo = Environment.CurrentDirectory + "\\RS.png";
             }
             else
-            if (a != b && a != c && b != c && a + b >= c && a + c >= b && b + c >= a  )
-            {               
-                    Result.Text = "Разносторонний треугольник";
-                    string photo = Environment.CurrentDirectory + "\\RasS.png";
-                    Uri uri = new Uri(photo, UriKind.Absolute);
-                    BitmapImage bitmapImage = new BitmapImage(uri);
-                    TR.Source = bitmapImage;                
-            }else
-            { 
-                if (a + b < c || a + c < b || b + c < a || a < 0 || b < 0 || c < 0)
-                {
-                    Result.Text = "Треугольник не существует ";
-                    TR.Source = null;
-                    
-                    string photo = Environment.CurrentDirectory + "\\N.png";
-                    Uri uri = new Uri(photo, UriKind.Absolute);
-                    BitmapImage bitmapImage = new BitmapImage(uri);
-                    TR.Source = bitmapImage;
-                }
+            if (a != b && a != c && b != c)
+            {
+                Result.Text = "Разносторонний треугольник";
+                photo = Environment.CurrentDirectory + "\\RasS.png";
             }
+            else
+            {
+                Result.Text = "Равнобедренный треугольник";
+                photo = Environment.CurrentDirectory + "\\RB.png";
+            }
+            Uri uri = new Uri(photo, UriKind.Absolute);
+            BitmapImage bitmapImage = new BitmapImage(uri);
+            TR.Source = bitmapImage;
         }
     }
 }
